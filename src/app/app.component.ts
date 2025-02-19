904eb845c7f6e01cfdd1c70c489fb6ae6d0a0e10
unknown <you@g00431947@atu.ie>
2025-02-19 11:12:42 +0000

    lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AComponent} from './a/a.component' //importing a componenet
+import { BComponent } from './b/b.component' //importing b component
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'root-directive',
+  imports: [RouterOutlet, AComponent, BComponent], //importing all the components
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'my-first-app'; //creating my values to use in databinding later
+  name: string = "Caoimhe"; //decalaring string name as Caoimhe
+  age: number = 19; //decalaring number age as 19
 }
+
